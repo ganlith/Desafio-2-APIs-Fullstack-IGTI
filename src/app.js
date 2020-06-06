5069895acddd5fc87468edd6ae4b0a0a36e940a1
Ganlith <marcelo.schoeffel@gmail.com>
2020-06-06 20:17:09 -0300

    Create App file

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require('cors');
+const { errors } = require('celebrate');
+const routes = require('./routes');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+module.exports = app;
+
